09c379f7e84bb4fbf7e9af0e20d0d38fb7a05ac5
Gcspidey43 <linkkdapet@gmail.com>
2026-01-08 18:19:10 +0500

    Fix pages.config.ts to use correct export format for @cloudflare/next-on-pages

--- pages.config.ts
+++ pages.config.ts
@@ -1,5 +1,4 @@
-import { defineConfig } from '@cloudflare/next-on-pages'
-
-export default defineConfig({
+// This file is used by Cloudflare Pages to configure the deployment
+export default {
   compatibilityDate: '2024-01-01',
-})
+}
